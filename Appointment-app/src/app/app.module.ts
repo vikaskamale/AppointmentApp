ce0efa74abc314b357e21131299876492212e38d
vikaskamale <vikas.kamale@valuemomentum.com>
2024-01-16 18:02:04 +0530

    first commit have created one component and perforemed interpolation

---
+++ Appointment-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppointmentListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
