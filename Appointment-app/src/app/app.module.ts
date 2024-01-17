c08ace3696cd9b2e0f74822deaabd1d66b47f52c
vikaskamale <vikas.kamale@valuemomentum.com>
2024-01-17 14:28:00 +0530

    Appointment-app created with add and delete functinality saving data in local storage

--- Appointment-app/src/app/app.module.ts
+++ Appointment-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppointmentListComponent } from './appointment-list/appointment-list.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { AppointmentListComponent } from './appointment-list/appointment-list.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
